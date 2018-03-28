b87630205deb013c594e611a1aeee17f4fad7b47
myeongju.jung <myeongju.jung@nhnent.com>
2018-03-28 21:20:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-edu-2018
---
+++ src/main/java/io/redutan/springboot/edu/SpringBootEducation2018Application.java
@@ -0,0 +1,12 @@
+package io.redutan.springboot.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEducation2018Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEducation2018Application.class, args);
+    }
+}
