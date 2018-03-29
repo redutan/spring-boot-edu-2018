f5d7029825f3cc186437ed16b410b74da95e53db
myeongju.jung <myeongju.jung@nhnent.com>
2018-03-29 23:10:25 +0900

    Actuator

---
+++ src/main/java/io/redutan/springboot/edu/acuator/CounterActuator.java
@@ -0,0 +1,23 @@
+package io.redutan.springboot.edu.acuator;
+
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author myeongju.jung
+ */
+@Component
+public class CounterActuator extends AbstractEndpoint<Long> {
+    private final AtomicLong counter = new AtomicLong();
+
+    public CounterActuator() {
+        super("counter");
+    }
+
+    @Override
+    public Long invoke() {
+        return counter.incrementAndGet();
+    }
+}
